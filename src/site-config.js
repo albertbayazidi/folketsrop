104c80b4b812063a0bb3f1b3eb09ad5f3377592c
Albert <albert.bayazidi@gmail.com>
2024-08-13 20:19:17 +0200

    inital commit

---
+++ src/site-config.js
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  author: 'Albert Bayazidi',
+  title: 'folketsrop.no',
+  subtitle: 'subtitle',
+  description: 'description',
+
+  navLinks: [
+      {
+        text: 'Cv',
+        href: '/Cv', 
+      },
+      {
+        text: 'Projects',
+        href: '/Projects',
+      },
+    ],
+}
+
+export default siteConfig;
