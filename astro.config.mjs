2e8997683b9585e0ec226d183910059360b45bec
Albert <albert.bayazidi@gmail.com>
2024-08-19 15:29:06 +0200

    added CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://albertbayazidi.github.io/folketsrop',
+
   integrations: [tailwind()]
 });
