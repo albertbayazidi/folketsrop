a4ea3f4b419e59db49e084b31b07f67454e95c6f
Albert <albert.bayazidi@gmail.com>
2024-08-19 14:50:35 +0200

    no base needed here. basiclly a copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://albertbayazidi.github.io/folketsrop',
-  base: 'folketsrop',
   integrations: [tailwind()]
 });
