8f7f4c602e0b8e1596fc9278c48a30b193e82b47
Albert <albert.bayazidi@gmail.com>
2024-11-04 20:31:46 +0100

    removed kultur page, added arabic categories

--- src/site-config.js
+++ src/site-config.js
@@ -5,30 +5,36 @@ export const siteConfig = {
   description: 'description',
 
   navLinks: [
-    {
-      text: 'Alt',
-      href: '/', 
-    },
-    {
-      text: 'Politikk',
-      href: '/politikk', 
-    },
-    {
-      text: 'Næringsliv',
-      href: '/naringsliv',
-    },
-    {
-      text: 'Utenriks',
-      href: '/utenriks',
-    },
-    {
-      text: 'Kultur',
-      href: '/kultur'
-    },
-    {
-      text: 'Om oss',
-      href: '/om_oss'
-    },
+{
+  text: 'Alt',
+  text_eng: 'Alt',
+  text_arb: 'البديل',
+  href: '/',
+},
+{
+  text: 'Politikk',
+  text_eng: 'Politics',
+  text_arb: 'السياسة',
+  href: '/politikk',
+},
+{
+  text: 'Næringsliv',
+  text_eng: 'Business',
+  text_arb: 'الأعمال',
+  href: '/naringsliv',
+},
+{
+  text: 'Utenriks',
+  text_eng: 'Foreign Affairs',
+  text_arb: 'الشؤون الخارجية',
+  href: '/utenriks',
+},
+{
+  text: 'Om oss',
+  text_eng: 'About Us',
+  text_arb: 'عنّا',
+  href: '/om_oss',
+},
 
   ],
 }
