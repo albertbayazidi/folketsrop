aed2ff5887e93d85fc45eae4877bc227f159d5f4
Albert <albert.bayazidi@gmail.com>
2024-11-05 16:54:13 +0100

    testing out new font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,11 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Liberation Sans"', 'sans-serif'], // Use "Liberation Sans" as a sans-serif font
+        serif: ['"Liberation Serif"', 'serif'],     // For serif variant
+        mono: ['"Liberation Mono"', 'monospace'],   // For monospaced variant
+       },
       spacing: {
 
       }
